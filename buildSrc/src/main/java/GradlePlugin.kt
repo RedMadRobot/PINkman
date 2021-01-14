dc23d1feeabe2e3a4aae2fd1fd5f48bcda1728e2
Artem Kulakov <ak@redmadrobot.com>
2021-01-14 12:30:28 +0300

    Update dependencies

--- buildSrc/src/main/java/GradlePlugin.kt
+++ buildSrc/src/main/java/GradlePlugin.kt
@@ -1,16 +1,16 @@
 object GradlePlugin {
     object DependencyCheck {
-        const val version = "6.0.2"
+        const val version = "6.0.5"
         const val plugin = "org.owasp.dependencycheck"
     }
 
     object DependencyVersions {
-        const val version = "0.33.0"
+        const val version = "0.36.0"
         const val plugin = "com.github.ben-manes.versions"
     }
 
     object Detekt {
-        const val version = "1.14.2"
+        const val version = "1.15.0"
         const val plugin = "io.gitlab.arturbosch.detekt"
 
         const val formattingPlugin = "io.gitlab.arturbosch.detekt:detekt-formatting:$version"
